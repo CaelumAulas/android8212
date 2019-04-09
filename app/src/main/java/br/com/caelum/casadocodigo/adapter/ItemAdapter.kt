c1070d8790321b89d04cc6855b89d5e18062aeb0
Matheus Brandino <m.v.brandino@gmail.com>
2019-04-08 22:02:51 -0300

    refactor do adapter

--- app/src/main/java/br/com/caelum/casadocodigo/adapter/ItemAdapter.kt
+++ app/src/main/java/br/com/caelum/casadocodigo/adapter/ItemAdapter.kt
@@ -14,31 +14,22 @@ import com.squareup.picasso.Picasso
 
 class ItemAdapter(private val itens: List<Item>) : RecyclerView.Adapter<ItemViewHolder>() {
 
-    override fun getItemCount(): Int {
-        return itens.size
-    }
+    override fun getItemCount(): Int = itens.size
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-
         val inflater = LayoutInflater.from(parent.context)
-
         if (viewType == 1) {
             val view = inflater.inflate(R.layout.item_livro_fisico, parent, false)
             return ItemFisicoVH(view)
-
         } else if (viewType == 2) {
             val view = inflater.inflate(R.layout.item_livro_virtual, parent, false)
             return ItemVirtualVH(view)
-
         } else {
             val view = inflater.inflate(R.layout.item_livro_ambos, parent, false)
             return ItemAmbosVH(view)
-
         }
-
     }
-
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = itens[position]
@@ -46,101 +37,68 @@ class ItemAdapter(private val itens: List<Item>) : RecyclerView.Adapter<ItemView
     }
 
     override fun getItemViewType(position: Int): Int {
-
         val item = itens[position]
-
         when (item.tipo) {
             TipoDeLivro.FISICO -> return 1
-
             TipoDeLivro.VIRTUAL -> return 2
-
             TipoDeLivro.AMBOS -> return 3
         }
-
     }
-
 }
 
-sealed class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+sealed class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    abstract fun bind(item: Item)
+    internal val itemPreco: TextView = view.findViewById(R.id.itemPreco)
+    private val itemNome: TextView = view.findViewById(R.id.itemNome)
+    private val itemFoto: ImageView = view.findViewById(R.id.itemFoto)
+
+    open fun bind(item: Item) {
+        itemNome.text = item.livro.nome
+
+        var url = item.livro.foto
+
+        if (url.startsWith("http:")) {
+            url = url.replace("http:", "https:")
+        }
+
+        Picasso.get().load(url).fit().into(itemFoto)
+    }
 
     class ItemFisicoVH(view: View) : ItemViewHolder(view) {
 
-        private val itemNome: TextView = view.findViewById(R.id.itemNome)
         private val itemISBN: TextView = view.findViewById(R.id.itemISBN)
-        private val itemPreco: TextView = view.findViewById(R.id.itemPreco)
-        private val itemFoto: ImageView = view.findViewById(R.id.itemFoto)
 
         override fun bind(item: Item) {
+            super.bind(item)
 
-
-            itemNome.text = item.livro.nome
             itemISBN.text = item.livro.isbn
             itemPreco.text = "R$ ${item.livro.precoFisico}"
 
-
-            var url = item.livro.foto
-
-            if (url.startsWith("http:")) {
-                url = url.replace("http:", "https:")
-            }
-
-            Picasso.get().load(url).fit().into(itemFoto)
         }
     }
 
     class ItemVirtualVH(view: View) : ItemViewHolder(view) {
 
-
-        private val itemNome: TextView = view.findViewById(R.id.itemNome)
         private val itemAutores: TextView = view.findViewById(R.id.itemAutores)
-        private val itemPreco: TextView = view.findViewById(R.id.itemPreco)
-        private val itemFoto: ImageView = view.findViewById(R.id.itemFoto)
-
 
         override fun bind(item: Item) {
+            super.bind(item)
 
-
-            itemNome.text = item.livro.nome
             itemAutores.text = "${item.livro.autores.map { autor -> autor.nome }}"
-            itemPreco.text = "R$ ${item.livro.precoFisico}"
-
-
-            var url = item.livro.foto
-
-            if (url.startsWith("http:")) {
-                url = url.replace("http:", "https:")
-            }
-
-            Picasso.get().load(url).fit().into(itemFoto)
+            itemPreco.text = "R$ ${item.livro.precoEbook}"
 
         }
     }
 
     class ItemAmbosVH(view: View) : ItemViewHolder(view) {
 
-        private val itemNome: TextView = view.findViewById(R.id.itemNome)
         private val itemDataPub: TextView = view.findViewById(R.id.itemDataPub)
-        private val itemPreco: TextView = view.findViewById(R.id.itemPreco)
-        private val itemFoto: ImageView = view.findViewById(R.id.itemFoto)
-
 
         override fun bind(item: Item) {
+            super.bind(item)
 
-
-            itemNome.text = item.livro.nome
             itemDataPub.text = item.livro.data
-            itemPreco.text = "R$ ${item.livro.precoFisico}"
-
-
-            var url = item.livro.foto
-
-            if (url.startsWith("http:")) {
-                url = url.replace("http:", "https:")
-            }
-
-            Picasso.get().load(url).fit().into(itemFoto)
+            itemPreco.text = "R$ ${item.livro.precoAmbos}"
 
         }
     }
